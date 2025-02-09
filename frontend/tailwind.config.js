bfe3310d570ebfcb226ef99bd43134366372724d
Surya Pratap <sps64044@gmail.com>
2025-02-09 19:19:04 +0530

    Fixed ESM issue in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
     './components/**/*.{js,jsx}',
     './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust as per your project structure
   ],
   prefix: "",
   theme: {
@@ -34,9 +34,47 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust as per your project structure
-  ],
 };
+
+// /** @type {import('tailwindcss').Config} */
+// export default = {
+//   darkMode: ["class"],
+//   content: [
+//     './pages/**/*.{js,jsx}',
+//     './components/**/*.{js,jsx}',
+//     './app/**/*.{js,jsx}',
+//     './src/**/*.{js,jsx}',
+//   ],
+//   prefix: "",
+//   theme: {
+//     container: {
+//       center: true,
+//       padding: "2rem",
+//       screens: {
+//         "2xl": "1400px",
+//       },
+//     },
+//     extend: {
+//       keyframes: {
+//         "accordion-down": {
+//           from: { height: "0" },
+//           to: { height: "var(--radix-accordion-content-height)" },
+//         },
+//         "accordion-up": {
+//           from: { height: "var(--radix-accordion-content-height)" },
+//           to: { height: "0" },
+//         },
+//       },
+//       animation: {
+//         "accordion-down": "accordion-down 0.2s ease-out",
+//         "accordion-up": "accordion-up 0.2s ease-out",
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// }
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // Adjust as per your project structure
+//   ],
+// };
